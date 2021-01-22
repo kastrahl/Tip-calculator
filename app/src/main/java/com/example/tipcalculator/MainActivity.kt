009d3594e09a45accb7e701de2af4e007696d77c
kastrahl <kastrahl@users.noreply.github.com>
2021-01-22 18:30:59 +0530

    completed kotlin code and set viewbidings and xml files , complete built code

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -7,40 +7,37 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         binding.calculateButton.setOnClickListener{ calculateTip() }
     }
     private fun calculateTip() {
 
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()             //converting string input to decimal number
+        val cost = stringInTextField.toDoubleOrNull()             //converting string input to decimal number
 
-        //getting tip options
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-
+        if (cost == null) {                 //to return the control and not let program crash
+            binding.tipResult.text = ""        //to reset the textview to display null tip
+            return
+        }
         //much easier to use when statement than if else
-        val tipPercentage = when (selectedId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-
         //checking if round up option if enabled
         val roundUp = binding.roundUpSwitch.isChecked
-
         if (roundUp) {                      //if true then the code proceeds to roundup
             tip = kotlin.math.ceil(tip)     //upper round up
         }
-
        // NumberFormat.getCurrencyInstance()    = formatting the tip
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)   //storing formatted tip
-
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
     }
